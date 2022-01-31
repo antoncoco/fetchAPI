d337e9d7b02a700a3465a8934c806884235c1786
antoncoco <cocoletziantonio@gmail.com>
2022-01-31 00:40:37 -0600

    Add loader to wait data and set error handling

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,29 @@
 import { getChuckJoke } from "../helpers/getChuckJoke.js";
 
 const cardText = document.querySelector('.card__text');
+const button = document.querySelector('.button');
 
 async function showCard() {
-  const {value} = await getChuckJoke();
-  cardText.innerText = value;
+  cardText.innerText = "Loading...";
+  cardText.classList.add("card__text--loading");
+  try {
+    const {value} = await getChuckJoke();
+    cardText.innerText = value;
+  } catch (error) {
+    cardText.innerText = "Upps! Something went wrong :(";
+    console.error(error);
+  }finally{
+    cardText.classList.remove("card__text--loading");
+  }
 }
 
 window.addEventListener('load', function(){
   showCard();
+});
+
+button.addEventListener('click', function(){
+  showCard();
 })
+
+
 
